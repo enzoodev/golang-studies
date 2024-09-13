3e6d5d7023115febb10d8170d47a91afde10b0f9
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-13 15:14:32 -0300

    refactor: Separate factorial function into its own file

--- functions/recursivity-simple/recursivity-simple.go
+++ functions/recursivity-simple/recursivity-simple.go
@@ -1,5 +1,18 @@
 package main
 
+func factorial(n uint) uint {
+	switch {
+		case n == 0:
+			return 1
+		default:
+			return n * factorial(n-1)
+	}
+}
+
 func main() {
-	
+	fatorialFour  := factorial(4)
+	println(fatorialFour)
+
+	fatorialZero  := factorial(0)
+	println(fatorialZero)
 }
