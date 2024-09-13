0e0ceb35048e86ca83482e8a530ce39d2cf86105
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-13 15:14:25 -0300

    refactor: Separate factorial function into its own file

--- functions/recursivity/recursivity.go
+++ functions/recursivity/recursivity.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+func factorial(n int) (int, error) {
+	switch {
+		case n < 0:
+			return 0, fmt.Errorf("invalid number: %d", n)
+		case n == 0:
+			return 1, nil
+		default:
+			value, _ := factorial(n-1)
+			return n * value, nil
+	}
+}
+
 func main() {
-	
+	fatorialFour, _  := factorial(4)
+	println(fatorialFour)
+
+	fatorialZero, _  := factorial(0)
+	println(fatorialZero)
+
+	_, err := factorial(-4)
+	if err != nil {
+		println(err)
+	}
 }
