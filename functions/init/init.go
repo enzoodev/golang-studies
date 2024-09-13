01fc70905dd862eb26c89748f59b8038048fa026
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-13 16:25:15 -0300

    feat: Add init functions to print messages

---
+++ functions/init/init.go
@@ -0,0 +1,5 @@
+package main
+
+func init() {
+	println("This is the init function")
+}
---
+++ functions/init/init2.go
@@ -0,0 +1,9 @@
+package main
+
+func init() {
+	println("This is the init function 2")
+}
+
+func main() {
+	println("This is the main function")
+}
