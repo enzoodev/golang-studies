b1d41cf7d172c7a0562a9caf701661c0b7574b93
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-13 14:44:12 -0300

    refactor: Separate closure function into its own file

--- functions/closure/closure.go
+++ functions/closure/closure.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+func closure() func() {
+	x := 10
+	
+	var f = func() {
+		println(x)
+	}
 
+	return f
+}
+
+func main() {
+	x := 20
+	println(x)
+
+	printX := closure()
+	printX()
 }
