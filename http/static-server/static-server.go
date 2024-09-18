d84ca5c69470d1cec4479d4020b6e617eb82bc2c
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-18 10:38:51 -0300

    Update static server port to 8080

--- http/static-server/static-server.go
+++ http/static-server/static-server.go
@@ -9,7 +9,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("public"))
 	http.Handle("/", fileServer)
 
-	const port = "3333"
+	const port = "8080"
 
 	log.Println("Server started at http://localhost:" + port)
 	log.Fatal(http.ListenAndServe(":" + port, nil)) 
